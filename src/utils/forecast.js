12e935f958a0a27ce078862574b63cc0324cb5b7
VitaliiZaichkin <vitalii.zaichkin@introduct.tech>
2022-06-06 09:51:43 +0300

    Adds humidity data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,26 +1,28 @@
-const request = require("postman-request");
+const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
     const url =
-        "http://api.weatherstack.com/current?access_key=11f038ad69987d6c60bbf3c99b3a1e0f&query=" +
+        'http://api.weatherstack.com/current?access_key=11f038ad69987d6c60bbf3c99b3a1e0f&query=' +
         latitude +
-        "," +
+        ',' +
         longitude;
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback("Unable to connect to weather service");
+            callback('Unable to connect to weather service');
         } else if (body.error) {
-            callback("Unable to find location");
+            callback('Unable to find location');
         } else {
             callback(
                 undefined,
                 body.current.weather_descriptions[0] +
-                    ". It is currently " +
+                    '. It is currently ' +
                     body.current.temperature +
-                    " degrees out. It feels like " +
+                    ' degrees out. It feels like ' +
                     body.current.feelslike +
-                    " degrees out."
+                    ' degrees out. The humidity is ' +
+                    body.current.humidity +
+                    '%.'
             );
         }
     });
