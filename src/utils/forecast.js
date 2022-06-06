16f74d4dea02f9c4c8e502c6d8ea0e93f57d0fc8
VitaliiZaichkin <vitalii.zaichkin@introduct.tech>
2022-06-06 08:51:19 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url =
+        "http://api.weatherstack.com/current?access_key=11f038ad69987d6c60bbf3c99b3a1e0f&query=" +
+        latitude +
+        "," +
+        longitude;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather service");
+        } else if (body.error) {
+            callback("Unable to find location");
+        } else {
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] +
+                    ". It is currently " +
+                    body.current.temperature +
+                    " degrees out. It feels like " +
+                    body.current.feelslike +
+                    " degrees out."
+            );
+        }
+    });
+};
+
+module.exports = forecast;
